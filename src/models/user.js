e45bd53ea6965c3fa71c918f62eb0ce69adaf0c6
ZarKoCool73 <dgonzales.dg7@gmail.com>
2024-08-17 12:34:46 -0500

    Actualizar schema de usuarios

--- src/models/user.js
+++ src/models/user.js
@@ -1,43 +1,14 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    lastname: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique:true
-    },
-    studentCode: {
-        type: String,
-        required: true
-    },
-    firstQuestion: {
-        type: String,
-        required: true
-    },
-    secondQuestion: {
-        type: String,
-        required: true
-    },
-    thirdQuestion: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    name: {type: String, required: true},
+    lastname: {type: String, required: true},
+    age: {type: Number, required: true},
+    email: {type: String, required: true, unique: true},
+    firstQuestion: {type: String, required: true},
+    secondQuestion: {type: String, required: true},
+    thirdQuestion: {type: String, required: true},
+    password: {type: String, required: true}
 });
 
 module.exports = mongoose.model('User', userSchema);
