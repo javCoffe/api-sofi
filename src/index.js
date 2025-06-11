17f9f205b9cc2257dadb5687eb6bd50e9035bf5b
ZarKoCool73 <dgonzales.dg7@gmail.com>
2025-06-11 13:27:43 -0500

    update variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require ("dotenv").config();
+require('dotenv').config();
+require('newrelic');
 
 const fs = require('fs');
 
